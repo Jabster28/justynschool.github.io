4629984cc693c1b16501d58e76639b6418f07199
Gistyn <29015942+Jabster28@users.noreply.github.com>
2018-09-18 21:10:35 +0100

    Finalized border settings

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 $(document).ready(function() {
   console.log("jQuery Ready");
-  (document.getElementsByTagName('table')[0]).className = 'notPostContent';
-  for (i = 0; i < document.getElementsByTagName('td').length; i++) {
-    document.getElementsByTagName('td')[i].className = 'notPostContent';
-  }
-  for (i = 0; i < document.getElementsByTagName('tr').length; i++) {
-    document.getElementsByTagName('tr')[i].className = 'notPostContent';
+  if ((document.location.pathname == "/index.html" || document.location.pathname == "/") === true) {
+    // This checks that the page is on the root directory or index.html
+    console.log("on index")
+    (document.getElementsByTagName('table')[0]).className = 'notPostContent';
+    for (i = 0; i < document.getElementsByTagName('td').length; i++) {
+      document.getElementsByTagName('td')[i].className = 'notPostContent';
+    }
+    for (i = 0; i < document.getElementsByTagName('tr').length; i++) {
+      document.getElementsByTagName('tr')[i].className = 'notPostContent';
+    }
   }
 
-  if ((document.location.pathname == "/index.html" || document.location.pathname == "/") === false) {
-    // This checks that the page is not on the root directory or index.html
-    console.log("not on index")
-  }
+
+
 
 });
