2e76472b99039232196451fe5fb067edf09d6dde
Gistyn <29015942+Jabster28@users.noreply.github.com>
2018-09-18 18:41:18 +0100

    Finalized Mini-Project

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 
 (document.getElementsByTagName('table')[0]).className = 'notPostContent';
-(document.getElementsByTagName('td')[0]).className = 'notPostContent';
-(document.getElementsByTagName('tr')[0]).className = 'notPostContent';
+for (i = 0; i < document.getElementsByTagName('td').length; i++) {
+  document.getElementsByTagName('td')[i].className = 'notPostContent';
+}
+for (i = 0; i < document.getElementsByTagName('tr').length; i++) {
+  document.getElementsByTagName('tr')[i].className = 'notPostContent';
+}
+
 if ((document.location.pathname == "/index.html" || document.location.pathname == "/") === false) {
     // This checks that the page is not on the root directory or index.html
   console.log("not on index")
