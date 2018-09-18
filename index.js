29173ea117e02936a2b45e2cd36bbce88c501fc5
Gistyn <29015942+Jabster28@users.noreply.github.com>
2018-09-18 21:27:13 +0100

    Added a neccesary semicolon

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
   console.log("jQuery Ready");
   if ((document.location.pathname == "/index.html" || document.location.pathname == "/") === true) {
     // This checks that the page is on the root directory or index.html
-    console.log("on index")
+    console.log("on index");
     (document.getElementsByTagName('table')[0]).className = 'notPostContent';
     for (i = 0; i < document.getElementsByTagName('td').length; i++) {
       document.getElementsByTagName('td')[i].className = 'notPostContent';
