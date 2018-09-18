0f710662fedfcfdac4dfe77be0946e2bd368d8e2
Gistyn <29015942+Jabster28@users.noreply.github.com>
2018-09-18 18:14:36 +0100

    ---

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 var table = document.getElementsByTagName('table')
 var td = document.getElementsByTagName('td')
 var tr = document.getElementsByTagName('tr')
-table.classList.add('notPostContent');
-td.classList.add('notPostContent');
-tr.classList.add('notPostContent');
+table.className = ('notPostContent');
+td.className = ('notPostContent');
+tr.className = ('notPostContent');
 if ((document.location.pathname == "/index.html" || document.location.pathname == "/") === false) {
     // This checks that the page is not on the root directory or index.html
   console.log("not on index")
-  document.getElementById("shorthand").classList.add('notPostConten');
+  document.getElementById("shorthand").className = ('notPostContent');
 }
