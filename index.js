0a12418e224138a69d9ceb6cf39940989a1ef376
Gistyn <29015942+Jabster28@users.noreply.github.com>
2018-09-18 17:52:33 +0100

    Fixed bracket problem

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-if ((document.location.pathname == "/index.html") || ((document.location.pathname == "/") === false) {
-  // This checks that the page is not on the root directory or index.html
+if ((document.location.pathname == "/index.html" || document.location.pathname == "/") === false) {
+    // This checks that the page is not on the root directory or index.html
   console.log("not on index")
 }
